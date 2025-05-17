12cba9a6a440a89aebf165df7e58e984c2866778
Rafael flores moreno <flores_freak@hotmail.com>
2025-05-17 13:34:10 -0700

    code changes

---
+++ StudentCrud/StudentCrud.Domain.Services/Contracts/IPhoneService.cs
@@ -0,0 +1,15 @@
+﻿using StudentCrud.Domain.Model.Common;
+using StudentCrud.Domain.Model.DatabaseModels;
+using System.Collections.Generic;
+
+namespace StudentCrud.Domain.Services.Contracts
+{
+    public interface IPhoneService
+    {
+        ResultTrack Add(Phone entity);
+        ResultTrack Update(Phone entity);
+        ResultTrack Delete(Phone entity);
+        IEnumerable<Phone> GetAll();
+        Phone GetBy(int phonr_Id);
+    }
+}
---
+++ StudentCrud/StudentCrud.Domain.Services/Implementations/PhoneService.cs
@@ -0,0 +1,39 @@
+﻿using StudentCrud.Domain.Model.Common;
+using StudentCrud.Domain.Model.DatabaseModels;
+using StudentCrud.Domain.Model.Repositories;
+using StudentCrud.Domain.Services.Contracts;
+using StudentCrud.Infrastucture.Database;
+using System.Collections.Generic;
+
+namespace StudentCrud.Domain.Services.Implementations
+{
+    public class PhoneService : IPhoneService
+    {
+        private readonly IPhoneRepository phoneRepository = new PhoneRepository();
+
+        public ResultTrack Add(Phone entity)
+        {
+            return phoneRepository.Add(entity);
+        }
+
+        public ResultTrack Delete(Phone entity)
+        {
+            return phoneRepository.Delete(entity);
+        }
+
+        public IEnumerable<Phone> GetAll()
+        {
+            return phoneRepository.GetAll();
+        }
+
+        public Phone GetBy(int phone_Id)
+        {
+            return phoneRepository.GetBy(phone_Id);
+        }
+
+        public ResultTrack Update(Phone entity)
+        {
+            return phoneRepository.Update(entity);
+        }
+    }
+}
